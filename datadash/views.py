b0f6d38aaa37a8c142c9670cd5943d832fd3b50c
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2024-03-13 15:28:21 -0300

    chore: atualiza custom view maneira dinamica

--- datadash/views.py
+++ datadash/views.py
@@ -68,55 +68,23 @@ class CustomViewSet(viewsets.ViewSet):
                 )
                 # Salva a instância de StationReadings no banco de dados
                 station_reading.save()
-
-                # JAMILSON !!!!
-                # Aqui inicia a parte que temos que pegar na model o 'code'
-                # Então, testar se na string de envio ex:
-                #
-                #
                 # curl -X POST  http://185.137.92.73:8080/recebe/custom/ -d '!BDBSD=UFP_03&data=8-3-2024_10_5&TA_MIN=14&TA_AVG=21&TA_MAX=32&VB=12.3&TW=112'
-                #
-                #
-                # veio alguma chave que corresponde. No exemplo do curl tem as chaves TA_MIN, TA_AVG, ...VB, TW
-                # Então, pegamos o id. O id será usado para gravar  na model sensor_id.
-                # Usaremos no reading_id o id da instancia de station_instance
+                print(dados)
+                # for i in dados.items():
+                #   print(f"var = {i[1]}")
+                for indice, (chave, valor) in enumerate(dados.items()):
+                    if indice > 1:
+                        print(f"Chave: {chave}, Valor: {valor}")
+                        station_sensor = StationSensors.objects.create(
+                            code=chave, station=station_instance
+                        )
 
-                # Lista para armazenar os IDs dos sensores encontrados
-                sensor_ids = []
-
-                # Itera sobre os dados para encontrar correspondências com a model StationSensors
-                # for key, value in dados.items():
-                for key in dados.items():
-                    # Verifica se o campo corresponde ao código ou nome do sensor
-                    print(
-                        key[0]
-                    )  # em key[1] está o valor. É necessário depois instanciar a ultima model
-                    # StationReadingsSensors será instanciada reading_id será igual para todos os sensores na leitura,
-                    #  sensor_id e o data_value = key[1]
-                    # if key in ['code', 'name']:
-                    if key in ["code"]:
-                        # Tenta encontrar a instância de StationSensors com base no código ou nome fornecido
-                        try:
-                            # sensor_instance = StationSensors.objects.get(station=station_instance, **{key: value})
-                            sensor_instance = StationSensors.objects.get(
-                                station=station_instance, code__iexact=key
-                            )
-                            print(
-                                sensor_instance
-                            )  # No servidor eu vejo os dados, mas não consigo instanciar aqui
-                            sensor_ids.append(sensor_instance.id)
-                        except StationSensors.DoesNotExist:
-                            print(f"Sensor não encontrado para a chave '{key}'")
-                            pass  # Lidar com o caso em que o sensor não é encontrado
-
-                # Verifica se algum sensor foi encontrado
-                if sensor_ids:
-                    return Response(sensor_ids)
-                else:
-                    return Response(
-                        {"message": "Nenhum sensor encontrado"},
-                        status=status.HTTP_404_NOT_FOUND,
-                    )
+                        station_reading_sensor = StationReadingsSensors.objects.create(
+                            reading=station_reading,
+                            sensor=station_sensor,
+                            data_value=valor,
+                        )
+                return Response(data_hora_formatada)
 
             except StationStation.DoesNotExist:
                 return Response(
