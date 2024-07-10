a7554785b1542d09549ca7b5038709288f8b3291
Mateus <mateus.santin@gmail.com>
2024-07-10 15:58:38 -0300

    PROJECT_DIR teste #4 fixei o diretório

--- gitwebhook/views.py
+++ gitwebhook/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 
-# gitwebhook/views.py
-
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 import hmac
 import hashlib
 import subprocess
 import os
+import logging
 
 SECRET = '6}ry[Qp2)0d,=hL_^8doM8NB1JZ,.'
-#PROJECT_DIR = os.getenv('PROJECT_DIR')  # Variável já carregada do .env
 PROJECT_DIR = '/home/alm_api/alm_api'  # Diretório fixo do projeto
+
+logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def webhook(request):
@@ -23,17 +23,20 @@ def webhook(request):
         if not hmac.compare_digest(signature, request.headers.get('X-Hub-Signature', '')):
             return HttpResponseForbidden('Forbidden')
 
-        # Altere para o diretório do projeto antes de executar o comando Git
-        #os.chdir('/home/alm_api/alm_api')
+        # Verifique se o diretório existe
+        if not os.path.exists(PROJECT_DIR):
+            logger.error(f"Invalid PROJECT_DIR: {PROJECT_DIR}")
+            return HttpResponse("Invalid PROJECT_DIR", status=500)
+
         logger.info(f"Changing directory to: {PROJECT_DIR}")
         os.chdir(PROJECT_DIR)
 
+        logger.info(f"Current working directory: {os.getcwd()}")
+        result = subprocess.run(['git', 'pull', 'origin', 'main'], capture_output=True, text=True)
 
-        # Comando Git para puxar as atualizações
-        subprocess.Popen(['git', 'pull', 'origin', 'main'])
-
-        # Comando para reiniciar o servidor, se necessário
-        # subprocess.Popen(['systemctl', 'restart', 'gunicorn'])
+        if result.returncode != 0:
+            logger.error(f"Git pull failed: {result.stderr}")
+            return HttpResponse(result.stderr, status=500)
 
         return HttpResponse(status=204)
     return HttpResponse(status=405)
