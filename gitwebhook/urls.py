9c2b7120c39fc65f01fd681c4930d7ebb2c332c1
Mateus <mateus.santin@gmail.com>
2024-07-19 11:16:28 -0300

    Ajustando path webhook #7

--- gitwebhook/urls.py
+++ gitwebhook/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.webhook, name="webhook"),  # Remova a parte "gitwebhook/" do caminho
+  path("gitwebhook/", views.webhook, name="webhook"),  # Corrigido para `gitwebhook/`
 ]
 
